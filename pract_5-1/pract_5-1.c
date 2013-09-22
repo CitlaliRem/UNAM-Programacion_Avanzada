122c54ebc08e9b2b9a4ebbba880b7bb10e99f5a2
mgs <magnus.henkel@zoho.com>
2013-09-22 13:36:00 -0500

    gitignore, eclipse stuff

---
+++ pract_5-1/pract_5-1.c
@@ -0,0 +1,20 @@
+/*
+ * pract_5-1.c
+ *
+ *  Created on: Sep 22, 2013
+ *      Authors: César Alberto Trejo Juárez, Magnus Henkel
+ *
+ *      Elaborar un programa que implemente la Eliminación Gaussiana para un
+ *      sistema de ecuaciones lineales de 3 x 3.
+ */
+
+
+#include<stdio.h>
+
+main() {
+
+}
+
+
+
+
---
+++ pract_5-2/pract_5-2.c
@@ -0,0 +1,20 @@
+/*
+ * pract_5-2.c
+ *
+ *  Created on: Sep 22, 2013
+ *      Authors: César Alberto Trejo Juárez, Magnus Henkel
+ *
+ *      Utilizando el programa anterior, resolver el siguiente sistema de
+ *      ecuaciones.
+ *
+ *		5x_1 + 3x_2 − 8x_3 = 85.3
+ *		−x_1 +4x_2 −6x_3 =14.32
+ *		4x_1 − 6x_2 + x_3 = 17.61
+ */
+
+#include<stdio.h>
+
+
+main() {
+
+}
