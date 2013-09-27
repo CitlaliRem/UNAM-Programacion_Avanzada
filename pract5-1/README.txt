2944c7e41cadb6de2dad6b613fd356f05b9a4857
mgs <magnus.henkel@zoho.com>
2013-09-27 10:16:01 -0500

    change columns

---
+++ pract5-1/README.txt
@@ -0,0 +1,19 @@
+
+>>>Nommbre:  pract5-1.c
+
+>>>Programadores:   
+-Magnus Henkel____magnus.henkel@zoho.com
+                
+-César Alberto Trejo Juárez____cesaratj27@gmail.com
+
+
+>>>Descripción:
+Programa que aplique la Eliminación Gausseana 
+
+>>>Consideraciones:
+
+
+Programación avanzada y métodos numéricos.
+Profesor:  Germán Santos Jaimes
+UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO
+FACULTAD DE INGENIERÍA
--- pract5-3/pract5-3.c
+++ pract5-3/pract5-3.c
@@ -10,21 +10,37 @@
 
 int main(){
 
-	int i, j ,k;
+	int i, j ,k, l;
+	int flag = 0;
 
 	float A[3][3] = {{1,2,3},{-2,-4,8},{3,1,2}};
 	float B[3] ={ 2.4,6.4,7.6};
+	float temp[3][3];
 	float cte;
 
 	for(i=0; i<3;i++){
 	    for(j=i+1; j<3; j++){
-	    	if (A[i][i]==0){
-				i++;
-			}
-			cte = A[j][i]/A[i][i];
-		
+
+
 			for(k=i; k<3; k++){
+				if(flag == 1) {
+					for(l=0;l<3;l++) {
+						temp[i][l] = A[i+1][l];
+						A[i+1][l] = A[i][l];
+						A[i][l] = temp[i][l];
+					}
+				}
+
+			cte = A[j][i]/A[i][i];
+
+
 				A[j][k] = A[j][k] - cte * A[i][k];
+
+				if (A[i][i]==0){
+					k=0;
+					flag = 1;
+					break;
+				}
 			}
 			B[j] = B[j] - cte * B[i];
 	    }
@@ -36,5 +52,8 @@ int main(){
 		}
 		printf("| %0.2f\n", B[i]);
 	}
+
+	printf("\nx_3 Value: %0.2f", B[2]/A[2][2]);
+
 	return 0;
 }
