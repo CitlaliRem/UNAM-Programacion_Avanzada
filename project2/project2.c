56a2011a79678f23afbf512b501c1d6a4587f7d9
loxosceles <loxosceles@users.noreply.github.com>
2013-10-02 20:38:06 -0500

    Update project2.c
    prequeño cambio en comentario

--- project2/project2.c
+++ project2/project2.c
@@ -141,7 +141,7 @@ void gaussSeidel() {
 	            threshold = threshold > (fabsf(x[i] - prev_x[i])) ? threshold : fabsf((x[i] - prev_x[i]));
 	        }
 
-	    } while(threshold > 0.000001);  /* preción */
+	    } while(threshold > 0.000001);  /* precisión */
 
 	    printf("Solution:\n");
 	    printf("x_1: %0.5f\t\n", x[0]);
