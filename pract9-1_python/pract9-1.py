7a0760402b728bc33e4c37d6d76d32c1d244833b
César Alberto <cesaratj27@gmail.com>
2013-10-27 20:42:30 -0600

    pract9-1.py Done

--- pract9-1_python/pract9-1.py
+++ pract9-1_python/pract9-1.py
@@ -1,19 +1,31 @@
 '''
 Created on Oct 20, 2013
  
-  productComplex.py
+  pract9-1.py
 
   Authors: 
-          César Alberto Trejo Juárez (cesaratj27 at gmail.com)
+          
           Magnus Henkel (magnus.henkel at zoho.com)
+          Cesar Alberto Trejo Juarez (cesaratj27 at gmail.com)
   
   Programa que recibe dos numeros complejos, y obtiene la multiplicacion.
   
 '''
 
-# importamos la libreria que permite trabajar con numeros complejos.
-import cmath
+class complex:
+	"""Para el producto de dos numeros complejos"""
+	def __init__(self, r, i):
+		self.real = r
+		self.img = i
 
-c1 = input('Write a complex number (a+bj): ')
-c2 = input('Write another complex number (c+dj): ')
-print 'The product is: ' + str(c1 * c2)
+	def mostrar(self):
+		print self.real, '+', self.img,'i'
+
+	def producto(self,z):
+		return complex( self.real * z.real - self.img * z.img, self.real * z.img + self.img * z.real)
+
+
+c1 = complex(2,-4)
+c2 = complex(-1,0)
+c3 = c1.producto(c2)
+c3.mostrar()
