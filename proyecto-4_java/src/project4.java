8094d6d0241db305f8dd57fd2fbccb380e567a79
César Alberto <cesaratj27@gmail.com>
2013-10-31 00:11:13 -0600

    Update project4.java

--- proyecto-4_java/src/project4.java
+++ proyecto-4_java/src/project4.java
@@ -21,7 +21,8 @@ public class project4{
 	*	Main
 	*/
 	public static void main(String args[]){
-		String cadena; 
+		String cadena;
+		int i; 
 		StringTokenizer str;
 		listado lisTado = new listado();
 
@@ -31,14 +32,21 @@ public class project4{
 			BufferedReader entrada = new BufferedReader(archivo);
 			while( (cadena = entrada.readLine()) != null ){
 				str = new StringTokenizer(cadena);
+				// cadena sera un elemento 0 de un arraylist
 				while( str.hasMoreTokens() ){
 					lisTado.lineOfFile.add( str.nextToken("|") );
 				}
 				lisTado.estado = lisTado.lineOfFile.get(4);
 				// funcion searchEdo
-				System.out.println(lisTado.lineOfFile.get(0));
-				System.out.println(lisTado.estado);
+				lisTado.searchEdo(lisTado.listaEdo,lisTado.estado);
+				//System.out.println(lisTado.lineOfFile.get(0));
+				//System.out.println(lisTado.estado);
+				lisTado.lineOfFile.clear();
 			}
+			//imprimir lista de estados sin repetir
+			Set inOrderEdo = new TreeSet(lisTado.listaEdo);
+			System.out.println(inOrderEdo);
+			System.out.println(lisTado.listaEdo.size());
 
 		}catch(FileNotFoundException var){
 			System.out.println("No existe el archivo");
