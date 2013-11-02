52a3b9a11f4e8923c5676d99a01f105ae103a26a
César Alberto <cesaratj27@gmail.com>
2013-11-01 22:03:07 -0600

    Update data.java

--- proyecto-4_java/src/data.java
+++ proyecto-4_java/src/data.java
@@ -1,15 +1,30 @@
-//import java.util.Objects;
+/**      
+ *     A simple class wich save the final data to print
+ * 
+ *      @authors: 
+ *      César Alberto Trejo Juárez (cesaratj27 at gmail.com)
+ *      Magnus Henkel (magnus.henkel at zoho.com)
+ *      
+ *          Created on: Oct 31, 2013
+ */
+import java.util.*;
 
 public class data{
 	public int idData;
 	public int cpCountedData;
-	public int delMunRanchData;
+	public int delMunData;
 	public String nameEdo;
 
-	public data(int id, String edo, int cp, int delMunRan){
+	public data(int id, String edo, int cp, int delMun){
 		this.idData = id;
 		this.nameEdo = edo;
 		this.cpCountedData = cp;
-		this.delMunRanchData = delMunRan;
+		this.delMunData = delMun;
 	}
+
+	@Override
+    public String toString() {
+
+        return "Estado: " + nameEdo + "\t\tNo CPs: " + cpCountedData + "\t\tNo Delegaciones/Municipios: "+ delMunData +"\n";
+    }
 }
