17fb8a87a32c17ed3ed8761097b55e0856381fd2
Skar241 <oscard241@gmail.com>
2013-11-29 02:30:38 -0600

    Agregando mensajes privados y usuarios en linea

--- proyecto_final/src/ClientHandler.java
+++ proyecto_final/src/ClientHandler.java
@@ -1,8 +1,3 @@
-/**
- * 
- *
- */
-
 import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
@@ -26,80 +21,81 @@ public class ClientHandler extends Thread{
     String inputString;
     ArrayList<String>  usersOnline = new ArrayList<String>();
     ArrayList <ClientHandler> clientCount = new  ArrayList <ClientHandler>();
-	private ArrayList<String> chatLog;
+        private ArrayList<String> chatLog;
 
-	public ClientHandler(Socket socket,ArrayList <ClientHandler> tmpClient, ArrayList<String> chatLog){
+        public ClientHandler(Socket socket,ArrayList <ClientHandler> tmpClient, ArrayList<String> chatLog,ArrayList tmpUsersOnline){
 
-		this.chatLog = chatLog;
-		this.clientSocket = socket;
-		this.clientCount = tmpClient;
-		numSockets = numSockets + 1;
-		id = numSockets;
-		//timeStamp = new SimpleDateFormat("dd-MM-yy HH:mm:ss");
-		timeStamp = new SimpleDateFormat("E MMM dd yyyy  HH:mm:ss  ");
-		System.out.println(this);
-	}
+                this.chatLog = chatLog;
+                this.clientSocket = socket;
+                this.clientCount = tmpClient;
+					 this.usersOnline = tmpUsersOnline;
+                numSockets = numSockets + 1;
+                id = numSockets;
+                //timeStamp = new SimpleDateFormat("dd-MM-yy HH:mm:ss");
+                timeStamp = new SimpleDateFormat("E MMM dd yyyy  HH:mm:ss  ");
+                System.out.println(this);
+        }
 
-	public void SignUp () {
-		try {
-			serverOutput.print("Choose your nickname: ");
-			String newNickname = userInput.readLine();
-		    serverOutput.print("Choose your password: ");
-		    String newPassword = userInput.readLine();
+        public void SignUp () {
+                try {
+                        serverOutput.print("Choose your nickname: ");
+                        String newNickname = userInput.readLine();
+                    serverOutput.print("Choose your password: ");
+                    String newPassword = userInput.readLine();
 
 
 
-			int tries = 0;
-			while(tries < 4) {
-	        	if (PasswordCheck.isValid(newPassword) == true) {
-	            	PasswordCheck.propSetter(newNickname, newPassword);
-	            	nickname = newNickname;
-	            	serverOutput.println("You can now log into your new account");
-	            	break;
-	
-	        	} else {
-	
-		        	if (tries == 3) {
-		            	serverOutput.println("Too many tries... Exiting");
-		        		userInput.close();
-						serverOutput.close();
-						clientSocket.close();
-						numSockets -= 1;
-		        	}
-	        		//System.out.println("LOG: " + time + " User choose invalid password " + (tries+1) + " times");
-	        		serverOutput.println("Password should be min 8 characters long and contain min 2 digits");
-	            	serverOutput.print("Choose your password: ");
-	            	newPassword = userInput.readLine();
-	
-	        		tries += 1;
-	
-	    		}
-			} 
-		}catch (IOException e) {
-			e.printStackTrace();
-		}  	
+                        int tries = 0;
+                        while(tries < 4) {
+                        if (PasswordCheck.isValid(newPassword) == true) {
+                            PasswordCheck.propSetter(newNickname, newPassword);
+                            nickname = newNickname;
+                            serverOutput.println("You can now log into your new account");
+                            break;
+        
+                        } else {
+        
+                                if (tries == 3) {
+                                    serverOutput.println("Too many tries... Exiting");
+                                        userInput.close();
+                                                serverOutput.close();
+                                                clientSocket.close();
+                                                numSockets -= 1;
+                                }
+                                //System.out.println("LOG: " + time + " User choose invalid password " + (tries+1) + " times");
+                                serverOutput.println("Password should be min 8 characters long and contain min 2 digits");
+                            serverOutput.print("Choose your password: ");
+                            newPassword = userInput.readLine();
+        
+                                tries += 1;
+        
+                            }
+                        } 
+                }catch (IOException e) {
+                        e.printStackTrace();
+                }          
 
     }
 
-	public void WriteToFile(ArrayList<String> chatLog) {
+        public void WriteToFile(ArrayList<String> chatLog) {
 /*
         for (int j = 0; j < chatLog.size(); j++) {
-        	String temp = chatLog.get(j);
-        	System.out.println(temp);
-		}
+                String temp = chatLog.get(j);
+                System.out.println(temp);
+                }
 */
-		PrintWriter logFile = null;
-		try {
-			logFile = new PrintWriter(new FileWriter("./chatlog.txt"));
-		} catch (IOException e) {
-			System.out.println("ERROR: Can't open logfile");
-		}
-		
-		for (int i = 0; i < chatLog.size(); i++) {
-			logFile.println(chatLog.get(i));
-		}
-		logFile.close();
-	}
+                PrintWriter logFile = null;
+                try {
+                        logFile = new PrintWriter(new FileWriter("./chatlog.txt"));
+                } catch (IOException e) {
+                        System.out.println("ERROR: Can't open logfile");
+                }
+                
+                for (int i = 0; i < chatLog.size(); i++) {
+                        logFile.println(chatLog.get(i));
+                }
+                logFile.close();
+        }
 
     public String toString() {
         return "ID : " + id + " ,Socket: " + numSockets;
@@ -110,7 +106,7 @@ public class ClientHandler extends Thread{
     public void run() {
         int i;
         String time = timeStamp.format(new Date());
-	
+        
         try {
             userInput = new DataInputStream(clientSocket.getInputStream());
             serverOutput = new PrintStream(clientSocket.getOutputStream());
@@ -124,32 +120,32 @@ public class ClientHandler extends Thread{
             Boolean signedUp = PasswordCheck.checkSignUp(nickname);
 
             if (signedUp == false) {
-            	serverOutput.println(nickname + " not found in database");
-            	serverOutput.println("Do you want to register? (y/n)");
-            	String option = userInput.readLine();
-            	if(! option.equals("y")) {
-            		userInput.close();
-    				serverOutput.close();
-    				clientSocket.close();
-    				numSockets -= 1;
-            	} else {
-	            	System.out.println("LOG: User choose option: " + option);
-	            	SignUp();
-            	}
+                    serverOutput.println(nickname + " not found in database");
+                    serverOutput.println("Do you want to register? (y/n)");
+                    String option = userInput.readLine();
+                    if(! option.equals("y")) {
+                            userInput.close();
+                                    serverOutput.close();
+                                    clientSocket.close();
+                                    numSockets -= 1;
+                    } else {
+                            System.out.println("LOG: User choose option: " + option);
+                            SignUp();
+                    }
             }
 
             serverOutput.print("Password: ");
             passwd = userInput.readLine();
 
-			Boolean validPass = PasswordCheck.checkCredentials(nickname, passwd);
-			
-			if (validPass == false) {
-				serverOutput.println("Sorry, you have no access to this chat");
-				userInput.close();
-				serverOutput.close();
-				clientSocket.close();
-				numSockets -= 1;
-			}
+                        Boolean validPass = PasswordCheck.checkCredentials(nickname, passwd);
+                        
+                        if (validPass == false) {
+                                serverOutput.println("Sorry, you have no access to this chat");
+                                userInput.close();
+                                serverOutput.close();
+                                clientSocket.close();
+                                numSockets -= 1;
+                        }
             nickname = Tools.capitalizeFirstLetter(nickname);
             
             if (usersOnline.contains(nickname)) {
@@ -159,63 +155,52 @@ public class ClientHandler extends Thread{
                 clientSocket.close();
                
             } else {
-            	serverOutput.println("\nSERVER:\tHi " + nickname + "\n>> : ");
+                    serverOutput.println("\nSERVER:\tHi " + nickname + "\n>> : ");
+						  usersOnline.add(nickname);
+						  //System.out.println(usersOnline);
             }
 
             serverOutput.println("\nSERVER:\tHi " + nickname + "\n>> : ");
 
             for(i=0; i<clientCount.size(); i++) {
                 if(clientCount.get(i)!=null && clientCount.get(i)!= this)
-                	clientCount.get(i).serverOutput.print("\n++ " + nickname  + " entered the room ++\n>> :");
+                        clientCount.get(i).serverOutput.print("\n++ " + nickname  + " entered the room ++\n>> :");
             }
 
             while(true) {
-            	serverOutput.print(">> : ");
+                    serverOutput.print(">> : ");
                 inputString = userInput.readLine();
                 if (! inputString.startsWith("/")) {
-                	time = timeStamp.format(new Date());
-                	chatLog.add(time + " " + nickname + ": " + inputString);
+                        time = timeStamp.format(new Date());
+                        chatLog.add(time + " " + nickname + ": " + inputString);
                 } else {
-                	System.out.println("String starting with /");
-                	System.out.println("String: " + inputString);
-                	UserCommands.CommandSwitch(serverOutput, inputString, nickname);
+                        System.out.println("String starting with /");
+                        System.out.println("String: " + inputString);
+                        UserCommands.CommandSwitch(serverOutput, inputString, nickname,clientCount,usersOnline);
                 }
 
                 if (UserCommands.ExitChat(serverOutput, inputString, nickname)) break;
-/////////////////////////////////aqui modifico un poco
-		if(inputString.startsWith("/private")){
-                    tmpInputString=inputString.substring(inputString.indexOf(' ')+1);
-                    tmpInputString = Tools.capitalizeFirstLetter(tmpInputString);
-                    if(usersOnline.indexOf(tmpInputString.substring(0,tmpInputString.indexOf(' ')))==-1){
-                        serverOutput.println("user not found");
-                    }
-                    else{
-                        clientCount.get(usersOnline.indexOf(tmpInputString.substring(0,tmpInputString.indexOf(' ')))).serverOutput.println(nickname+">> "+tmpInputString.substring(tmpInputString.indexOf(' ')));
-                    }
-					
-                }
 //////////////////////////////////////////////////////terminan cambios sinificativos la siguiente linea tambien esta mdificada  
-                if(!inputString.startsWith("/private")){
                 for(i=0; i<clientCount.size(); i++) {
                     if(clientCount.get(i)!=null && clientCount.get(i)!= this) {
-                    	clientCount.get(i).serverOutput.print("\n" + time + " " + nickname +": " + inputString + "\n>> : ");
+                            clientCount.get(i).serverOutput.print("\n" + time + " " + nickname +": " + inputString + "\n>> : ");
                     }
-                }}
+                }
             }
 
 
             for(i=0; i<clientCount.size();i++){
                 if(clientCount.get(i)!=null && clientCount.get(i)!= this)
-                	clientCount.get(i).serverOutput.print("\n++ " + nickname + " left ++\n>> : ");
-                	userInput.close();
-		            serverOutput.close();
-		            clientSocket.close();
+                        clientCount.get(i).serverOutput.print("\n++ " + nickname + " left ++\n>> : ");
+                        userInput.close();
+                            serverOutput.close();
+                            clientSocket.close();
             }
             
             
             for(i=0; i<clientCount.size(); i++) {
                 if(clientCount.get(i) == this) clientCount.set(i,null);///tengo duda en esta linea de codigo, para 
-					 //que la estamos implementando
+                                         //que la estamos implementando
             }
             
             WriteToFile(chatLog);
