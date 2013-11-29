8ec88863a5876fec3c21ee2283e56e765be37c9a
Skar241 <oscard241@gmail.com>
2013-11-29 02:32:02 -0600

    Actualizando

--- proyecto_final/src/Server.java
+++ proyecto_final/src/Server.java
@@ -1,17 +1,3 @@
- /* 	Proyecto Final - Programación Avanzada y Métodos Numéricos
- *  Semestre 14-1
- *  
- * 	Chat Server con los siguientes funcionalidades:	 
- *  1. login y password
- *	2. bitacora de pláticas (chatlog)
- *	3. lista de usuarios conectados (/showUsers)
- *	4. mensajes privados (/msg)
- *	5. Bloqueo de usuarios
- *	6. cliente automático (opcional)
- *
- *
- */
-
 import java.io.*;
 import java.net.*;
 import java.util.*; 
@@ -20,29 +6,30 @@ public class Server{
 
 public static final int PORT = 8888;
 
-	static Socket client;
+        static Socket client;
     static ArrayList<ClientHandler> listOfClients = new ArrayList<ClientHandler>();
-    static ArrayList<String> chatLog = new ArrayList<String>(); 
+    static ArrayList<String> chatLog = new ArrayList<String>();
+	 static ArrayList usersOnline = new ArrayList(); 
     
     
-	public static void recallHistory(ArrayList<String> arrayList) {
-		Scanner historyLog = null;
-		try {
-			historyLog = new Scanner(new File("./chatLog.txt"));
-			historyLog.useDelimiter("\n");
-		} catch (FileNotFoundException e) {
-			System.out.println("ERROR: Logfile not found");
-		}
-		
-		while (historyLog.hasNext()) {
-			String line = historyLog.next();
-			chatLog.add(line);
-		}
-		historyLog.close();
-	}
+        public static void recallHistory(ArrayList<String> arrayList) {
+                Scanner historyLog = null;
+                try {
+                        historyLog = new Scanner(new File("./chatLog.txt"));
+                        historyLog.useDelimiter("\n");
+                } catch (FileNotFoundException e) {
+                        System.out.println("ERROR: Logfile not found");
+                }
+                
+                while (historyLog.hasNext()) {
+                        String line = historyLog.next();
+                        chatLog.add(line);
+                }
+                historyLog.close();
+        }
 
-	
-	
+        
+        
     public static void main(String args[]){
 
         try{
@@ -54,27 +41,27 @@ public static final int PORT = 8888;
             while(true){
                    
                 client = chatServer.accept();
-                ClientHandler clientObj = new ClientHandler(client,listOfClients, chatLog);
+                ClientHandler clientObj = new ClientHandler(client,listOfClients, chatLog,usersOnline);
                 listOfClients.add(clientObj); //agregamos el cliente objeto a la lista dentro de la clase
                 clientObj.start(); //clientHandler
                 
             }
             
-            /* TODO: este cÃ³digo espera una entrada del usuario para salir
+            /* TODO: este cÃƒÂ³digo espera una entrada del usuario para salir
              * Para que funcione necesitamos que chatServer.accept() trabaje el en background 
              * porque de otra forma esta esperando a dos eventos a la vez: la
              * entrada del usuario y un nuevo cliente que se conecte
              * 
-	            System.out.println("Type 'q' for quitting the server");
-	            Scanner userInterrupt = new Scanner(System.in);
-	            shutdown = userInterrupt.next();
-	            System.out.println(shutdown);
+                    System.out.println("Type 'q' for quitting the server");
+                    Scanner userInterrupt = new Scanner(System.in);
+                    shutdown = userInterrupt.next();
+                    System.out.println(shutdown);
 
-            	if(shutdown == "q") {
-            		System.out.println("Exiting the server");
-            		System.exit(0);
-            	}
-            	*/
+                    if(shutdown == "q") {
+                            System.out.println("Exiting the server");
+                            System.exit(0);
+                    }
+                    */
         }catch(Exception var){
             System.out.println("An exception ocurred");
             System.out.println(var);
