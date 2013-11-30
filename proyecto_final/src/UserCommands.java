e04f4e0993ecc55bfd4e79964ca487e11461bde2
Skar241 <oscard241@gmail.com>
2013-11-30 15:37:37 -0600

    quitando comandos inecesarios

--- proyecto_final/src/UserCommands.java
+++ proyecto_final/src/UserCommands.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class UserCommands {
         
-        public static void CommandSwitch(PrintStream serverOutput, String inputString, String nickname,ArrayList <ClientHandler> clientCount, ArrayList <String> usersOnline,ArrayList usersBanned) {
+        public static void CommandSwitch(PrintStream serverOutput, String inputString, String nickname,ArrayList <ClientHandler> clientCount, ArrayList <String> usersOnline) {
                 System.out.println("Debugging: inputString " + inputString);
                 if (inputString.startsWith("/users")) {
                         System.out.println("si entro a users");
@@ -18,14 +18,6 @@ public class UserCommands {
                 else if(inputString.startsWith("/private")){
         		System.out.println("si entro a private");                
 			UserCommands.PrivateMenssage(serverOutput,inputString,nickname,clientCount,usersOnline);
-                }
-                else if(inputString.startsWith("/block")){
-                        System.out.println("si entro en block");                
-                        UserCommands.BlockUser(serverOutput,inputString,usersBanned);
-                }
-                else if(inputString.startsWith("/unlock")){
-                        System.out.println("si entro a unlock");                
-                        UserCommands.UnlockUser(serverOutput,inputString,usersBanned);
                 }
         }
 
@@ -76,28 +68,4 @@ public class UserCommands {
                 }                    
                 
         }
-        public static void BlockUser(PrintStream serverOutput, String inputString,ArrayList<String> usersBanned){
-                String tmpInputString=inputString.substring(inputString.indexOf(' ')+1);
-                tmpInputString = Tools.capitalizeFirstLetter(tmpInputString);
-                //serverOutput.println(tmpInputString.substring(tmpInputString.indexOf(' ')+1));
-                //System.out.println(tmpInputString.substring(tmpInputString.indexOf(' ')+1));
-                if(tmpInputString.substring(tmpInputString.indexOf(' ')+1).equals("123456")){
-                        usersBanned.add(tmpInputString.substring(0,tmpInputString.indexOf(' ')));
-                        serverOutput.println("DONE ");
-                }else{
-                        serverOutput.println("ERROR: chose your pasword");
-                        //serverOutput.println(tmpInputString.substring(tmpInputString.indexOf(' ')+1));
-                }
-        }
-        public static void UnlockUser(PrintStream serverOutput, String inputString,ArrayList usersBanned){
-                String tmpInputString=inputString.substring(inputString.indexOf(' ')+1);
-                tmpInputString = Tools.capitalizeFirstLetter(tmpInputString);
-                if(tmpInputString.substring(tmpInputString.indexOf(' ')+1).equals("123456")){        
-                        usersBanned.remove(tmpInputString.substring(0,tmpInputString.indexOf(' ')));
-                        serverOutput.println("DONE");
-                }else{
-                        serverOutput.println("ERROR: chose your pasword aqui esta entrando");
-                }
-        }
-
 }
