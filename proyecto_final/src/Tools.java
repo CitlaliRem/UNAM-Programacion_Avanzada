c4f2c900b87153446ba69c487d405f0422ba8ac9
Skar241 <oscard241@gmail.com>
2013-12-02 03:26:34 -0600

    Actualizando

--- proyecto_final/src/Tools.java
+++ proyecto_final/src/Tools.java
@@ -1,13 +1,66 @@
-/**
- * 
- */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Properties;
+
 
 public class Tools {
 
-	public static String capitalizeFirstLetter(String original){
-	    if(original.length() == 0)
-	        return original;
-	    return original.substring(0, 1).toUpperCase() + original.substring(1);
-	}
-	
+        public static String capitalizeFirstLetter(String original){
+            if(original.length() == 0)
+                return original;
+            return original.substring(0, 1).toUpperCase() + original.substring(1);
+        }
+        
+        static boolean checkFileEntry(String nick, String password, String datafile) { 
+            try {
+                        File file = new File(datafile);
+                        FileInputStream fileInput = new FileInputStream(file);
+                        Properties properties = new Properties();
+                        properties.loadFromXML(fileInput);
+                        fileInput.close();
+
+                        if (properties.getProperty(nick) == null) return false;
+
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                
+        return true;  
+        }
+
+        public static void propSetter(String nick, String password, String datafile, String comment) {
+                try {
+                        File file = new File(datafile);
+                        FileInputStream fileInput = new FileInputStream(file);
+                        Properties properties = new Properties();
+                        properties.loadFromXML(fileInput);
+                        fileInput.close();
+
+                        properties.setProperty(nick, password);
+
+                        FileOutputStream fileOut = new FileOutputStream(file);
+                        properties.storeToXML(fileOut, comment);
+                        fileOut.close();
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+          }
+
+        public static void WriteToFile(ArrayList<String> chatLog, String historyLog) {
+                PrintWriter logFile = null;
+                try {
+                        logFile = new PrintWriter(new FileWriter(historyLog));
+                } catch (IOException e) {
+                        System.out.println("ERROR: Can't open logfile");
+                }
+                
+                for (int i = 0; i < chatLog.size(); i++) {
+                        logFile.println(chatLog.get(i));
+                }
+                logFile.close();
+        }
 }
