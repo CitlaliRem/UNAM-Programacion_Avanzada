c10b6411259faf5e89a6ed4f4fbec7f99645824a
Skar241 <oscard241@gmail.com>
2013-12-02 14:29:16 -0600

    Actualizando

--- proyecto_final/src/AdminActions.java
+++ proyecto_final/src/AdminActions.java
@@ -1,87 +1,88 @@
 /**
- * 
+ *
  *
  */
 import java.io.*;
 import java.util.*;
 
 public class AdminActions{
-	//ArrayList usersBanned = new ArrayList();
-	String accion;
-	String tmpAccion;
+        //ArrayList usersBanned = new ArrayList();
+        private static final String BLACKLIST = "./blacklist.xml"; 
+        String accion;
+        String tmpAccion;
+        String tmpAccionCap;
 
-	/*public AdminActions(ArrayList tmpUsersBanned){
-		this.usersBanned = tmpUsersBanned;
 
-	}*/
+        public static void ShowBlockedUsers(HashSet<String> blockedUsers) {
+                HashSet<String> usersBanned= Server.usersBanned;
 
-	public void SaveFile(String toFileWrite,ArrayList<String> usersBanned) {
-/*
-        for (int j = 0; j < chatLog.size(); j++) {
-                String temp = chatLog.get(j);
-                System.out.println(temp);
+                if(usersBanned.isEmpty()) {
+                        Server.readFileIntoArray(usersBanned, BLACKLIST);
                 }
-*/
-                PrintWriter logFile = null;
-                try {
-                        logFile = new PrintWriter(new FileWriter(toFileWrite));
-                } catch (IOException e) {
-                        System.out.println("ERROR: Can't open logfile");
+
+                for(String temp : usersBanned){
+                        System.out.println(temp);
                 }
-                
-                for (int i = 0; i < usersBanned.size(); i++) {
-                        logFile.print("*");
-                        logFile.print(usersBanned.get(i));
-                        logFile.println("#");
+        }
+        
+
+        public boolean action(){
+                BufferedReader order = new BufferedReader(new InputStreamReader(System.in));
+                while(true){
+                        try{
+                                System.out.println("Options: block (b) /unblock (u) /show blocked users (s) /quit (q)");
+                                System.out.print("Type option: ");
+                                accion = order.readLine();
+                                if(accion.startsWith("b")) {
+                                        String reoffending = "0";
+                                        System.out.println("Name of user to be blocked: ");
+                                        String blockUser = order.readLine();
+                                        //tmpAccion = blockUser.substring(blockUser.indexOf(' ')+1);
+                                        Tools.capitalizeFirstLetter(blockUser);
+                                        System.out.println(blockUser + " banned");
+
+                                        Tools.propSetter(blockUser, reoffending, BLACKLIST , "blocked Users");
+                                        Server.usersBanned.add(blockUser);
+
+                                        return true;
+
+                                } else if(accion.startsWith("u")) {
+                                        System.out.println("Name of user to be unblocked: ");
+                                        String unBlockUser = order.readLine();
+                                        System.out.println("Unblock user: " + unBlockUser);
+                                        String reoffending = "0"; // variable que se podría usar para la cuenta cuantas veces fue bloqueado
+
+                                        //tmpAccion = unBlockUser.substring(unBlockUser.indexOf(' ')+1);
+                                        System.out.println("unblock user after tmpAction" + unBlockUser);
+                                        String tmpAccionCap = Tools.capitalizeFirstLetter(unBlockUser);
+
+                                        if(Server.usersBanned.contains(unBlockUser)) {
+                                                System.out.println(unBlockUser + " unblocked");
+                                                Tools.propEraser(unBlockUser, BLACKLIST);
+                                                Server.usersBanned.remove(unBlockUser);
+
+                                        } else if(Server.usersBanned.contains(tmpAccionCap)) {
+                                                System.out.println(tmpAccion + " unblocked");
+                                                Tools.propEraser(tmpAccion, BLACKLIST);
+                                                Server.usersBanned.remove(tmpAccionCap);
+                                        } else {
+                                                System.out.println("User is not blocked");
+                                        }
+
+                                        return true;
+
+                                } else if (accion.startsWith("s")) {
+                                        ShowBlockedUsers(Server.usersBanned);
+                                }
+                                else if(accion.equals("q")){
+                                        return false;
+                                }
+                                //SaveFile("./banned.txt",Server.usersBanned);
+                        }catch(IOException e){
+                                System.out.println("ERROR:");
+                        }
                 }
-                logFile.close();
+
         }
 
-	public boolean action(){
-		BufferedReader order = new BufferedReader(new InputStreamReader(System.in));
-		//System.out.println("****welcome to admin panel***");
-		while(true){
-		try{
-			accion = order.readLine();
-			System.out.println("Your order is: "+accion);
-			if(accion.startsWith("block")){
-				tmpAccion = accion.substring(accion.indexOf(' ')+1);
-				tmpAccion = Tools.capitalizeFirstLetter(tmpAccion);
-				System.out.println(tmpAccion+" Banned");
-				Server.usersBanned.add(tmpAccion);
-				SaveFile("./banned.txt",Server.usersBanned);
-				return true;
-
-			}
-			else if(accion.startsWith("unlock")){
-				tmpAccion = accion.substring(accion.indexOf(' ')+1);
-				tmpAccion = Tools.capitalizeFirstLetter(tmpAccion);
-				if(Server.usersBanned.contains(tmpAccion)){
-					System.out.println(tmpAccion+" Done");
-					Server.usersBanned.remove(tmpAccion);
-					SaveFile("./banned.txt",Server.usersBanned);
-				}
-				else{
-					System.out.println("ERROR: User not found");
-				}
-				return true;
-			}
-			else if(accion.equals("q")){
-				return false;
-			}
-			else if(accion.equals("show")){
-				System.out.println("Users On:");
-				System.out.println(Server.usersOnline);
-				System.out.println("Banned Users:");
-				System.out.println(Server.usersBanned);
-			}
-			//SaveFile("./banned.txt",Server.usersBanned);
-		}catch(IOException e){
-				System.out.println("ERROR:");
-			}
-		}
-
-	}
-
 }
-
