535441b8a99417dad40eddb5693448fa121458ca
Skar241 <oscard241@gmail.com>
2013-12-02 17:22:55 -0600

    Actualizando

--- proyecto_final/src/Tools.java
+++ proyecto_final/src/Tools.java
@@ -1,66 +1,118 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Properties;
 
 
 public class Tools {
 
-	public static String capitalizeFirstLetter(String original){
-	    if(original.length() == 0)
-	        return original;
-	    return original.substring(0, 1).toUpperCase() + original.substring(1);
-	}
-	
-	static boolean checkFileEntry(String nick, String password, String datafile) { 
-    	try {
-			File file = new File(datafile);
-			FileInputStream fileInput = new FileInputStream(file);
-			Properties properties = new Properties();
-			properties.loadFromXML(fileInput);
-			fileInput.close();
+        public static String capitalizeFirstLetter(String original){
+            if(original.length() == 0)
+                return original;
+            return original.substring(0, 1).toUpperCase() + original.substring(1);
+        }
+        
+        static boolean checkFileEntry(String searchItem, String datafile) { 
+            try {
+                        File file = new File(datafile);
+                        FileInputStream fileInput = new FileInputStream(file);
+                        Properties properties = new Properties();
+                        properties.loadFromXML(fileInput);
+                        fileInput.close();
 
-			if (properties.getProperty(nick) == null) return false;
+                        if (properties.getProperty(searchItem) == null) {
+                                        System.out.println(searchItem + " does not exist");
+                                        return false;
+                        }
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                
         return true;  
-	}
+        }
+        
+        public static void readPropsToArray(HashSet<String> usersBanned, String datafile) {
+                try {
+                        File file = new File(datafile);
+                        FileInputStream fileInput = new FileInputStream(file);
+                        Properties properties = new Properties();
+                        properties.loadFromXML(fileInput);
+                        fileInput.close();
 
-	public static void propSetter(String nick, String password, String datafile, String comment) {
-		try {
-			File file = new File(datafile);
-			FileInputStream fileInput = new FileInputStream(file);
-			Properties properties = new Properties();
-			properties.loadFromXML(fileInput);
-			fileInput.close();
+              Enumeration<?> enumeration = properties.propertyNames();
 
-			properties.setProperty(nick, password);
+              while (enumeration.hasMoreElements()) {
+                      usersBanned.add((String) enumeration.nextElement());
+              }
+/* 
+              for (int i = 0; i < usersBanned.size(); i++) {
+                                System.out.println(usersBanned.get(i));
+                        }
+*/
 
-			FileOutputStream fileOut = new FileOutputStream(file);
-			properties.storeToXML(fileOut, comment);
-			fileOut.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	  }
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+        }
 
-	public static void WriteToFile(ArrayList<String> chatLog, String historyLog) {
-		PrintWriter logFile = null;
-		try {
-			logFile = new PrintWriter(new FileWriter(historyLog));
-		} catch (IOException e) {
-			System.out.println("ERROR: Can't open logfile");
-		}
-		
-		for (int i = 0; i < chatLog.size(); i++) {
-			logFile.println(chatLog.get(i));
-		}
-		logFile.close();
-	}
+        public static void propSetter(String keyVal, String propertyVal, String datafile, String comment) {
+                try {
+                        File file = new File(datafile);
+                        FileInputStream fileInput = new FileInputStream(file);
+                        Properties properties = new Properties();
+                        properties.loadFromXML(fileInput);
+                        fileInput.close();
+
+                        properties.setProperty(keyVal, propertyVal);
+
+                        FileOutputStream fileOut = new FileOutputStream(file);
+                        properties.storeToXML(fileOut, comment);
+                        fileOut.close();
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+          }
+
+        public static void propEraser(String keyVal, String datafile) {
+                try {
+                        File file = new File(datafile);
+                        FileInputStream fileInput = new FileInputStream(file);
+                        Properties properties = new Properties();
+                        properties.loadFromXML(fileInput);
+                        fileInput.close();
+                        //System.out.println("keyVal "+ keyVal);
+
+                        properties.remove(keyVal);
+
+                        FileOutputStream fileOut = new FileOutputStream(file);
+                        properties.storeToXML(fileOut, null);
+                        fileOut.close();
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+          }
+
+        public static void WriteToFile(ArrayList<String> chatLog, String historyLog) {
+                PrintWriter logFile = null;
+                try {
+                        logFile = new PrintWriter(new FileWriter(historyLog));
+                } catch (IOException e) {
+                        System.out.println("ERROR: Can't open logfile");
+                }
+                
+                for (int i = 0; i < chatLog.size(); i++) {
+                        logFile.println(chatLog.get(i));
+                }
+                logFile.close();
+        }
 }
