03c51ec2125210a2115f0037b4ff5d882bca78e7
Skar241 <oscard241@gmail.com>
2013-12-02 17:19:31 -0600

    Actualizando

--- proyecto_final/src/AccessTools.java
+++ proyecto_final/src/AccessTools.java
@@ -6,136 +6,115 @@ import java.net.Socket;
 
 public class AccessTools { 
 
-	private static final String USER_DATABASE = "users.xml";
+        private static final String USER_DATABASE = "users.xml";
 
-	static boolean checkSignUp(String nick) {
+        public static void SignUp (PrintStream serverOutput, DataInputStream userInput, Socket socket, String nickname, int numSockets) throws IOException {
+                String newNickname = null;
+                String newPassword = null;
 
-			try {
-				File file = new File(USER_DATABASE);
-				FileInputStream fileInput = new FileInputStream(file);
-				Properties properties = new Properties();
-				properties.loadFromXML(fileInput);
-				fileInput.close();
-	
-				String userName = properties.getProperty(nick);
+                if(! socket.isClosed()) {
+                        serverOutput.println(nickname + " not found in database");
+                        serverOutput.println("Do you want to register? (y/n)");
 
-				if(userName == null) {
-					System.out.println("LOG: Nickname does not exist");
-					return false;
-				}
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-		return true;
-	}
+                        String option = userInput.readLine();
 
-	public static void SignUp (PrintStream serverOutput, DataInputStream userInput, Socket socket, String nickname, int numSockets) throws IOException {
-		String newNickname = null;
-		String newPassword = null;
+                        if(! option.equals("y")) {
+                                numSockets -= 1;
+                                serverOutput.println("SERVER:\tGoodbye");
+                                ClientThread.closeConnections(userInput, serverOutput, socket);
 
-	 	serverOutput.println(nickname + " not found in database");
-    	serverOutput.println("Do you want to register? (y/n)");
+                        } else {
+                                System.out.println("LOG: User choose option: " + option);
+                                System.out.println("inside else: " + userInput);
+                                serverOutput.print("Choose your nickname: ");
+                                newNickname = userInput.readLine();
+                                serverOutput.print("Choose your password: ");
+                                newPassword = userInput.readLine();
+                        }
 
-    	String option = userInput.readLine();
+                        int tries = 0;
+                        while(tries < 4) {
+                                try {
 
-    	if(! option.equals("y")) {
-    		numSockets -= 1;
-    		
-    		ClientThread.closeConnections(userInput, serverOutput, socket);
-    		
-    	} else {
-        	System.out.println("LOG: User choose option: " + option);
-        	System.out.println("inside else: " + userInput);
-			serverOutput.print("Choose your nickname: ");
-			newNickname = userInput.readLine();
-		    serverOutput.print("Choose your password: ");
-			newPassword = userInput.readLine();
-    	}
+                                        if (AccessTools.isValid(newPassword) == true) {
+                                                Tools.propSetter(newNickname, newPassword, USER_DATABASE, "User Access");
+                                                nickname = newNickname;
+                                                serverOutput.println("You can now log into your new account");
+                                                break;
 
-			int tries = 0;
-			while(tries < 4) {
-				try {
-					
-	        	if (AccessTools.isValid(newPassword) == true) {
-	            	Tools.propSetter(newNickname, newPassword, USER_DATABASE, "User Access");
-	            	nickname = newNickname;
-	            	serverOutput.println("You can now log into your new account");
-	            	break;
-	
-	        	} else {
-	
-		        	if (tries == 3) {
-		            	serverOutput.println("Too many tries... Exiting");
-		            	ClientThread.closeConnections(userInput, serverOutput, socket);
-						numSockets -= 1;
-		        	}
-	        		//System.out.println("LOG: " + time + " User choose invalid password " + (tries+1) + " times");
-	        		serverOutput.println("Password should be min 8 characters long and contain min 2 digits");
-	            	serverOutput.print("Choose your password: ");
-	            	//newPassword = ClientHandler.RecordUserInput(userInput);
-	            	newPassword = userInput.readLine();
-	
-	        		tries += 1;
-	
-	    		}
-				} catch(NullPointerException e) {
-					/* respuesta era un enter */
-					
-				}
-			} 
-    }
-	
-    static boolean checkCredentials(String nick, String password) { 
+                                        } else {
 
-    	if (password == null) return false;
+                                                if (tries == 3) {
+                                                        serverOutput.println("Too many tries... Exiting");
+                                                        ClientThread.closeConnections(userInput, serverOutput, socket);
+                                                        numSockets -= 1;
+                                                }
+                                                //System.out.println("LOG: " + time + " User choose invalid password " + (tries+1) + " times");
+                                                if (! socket.isClosed()) {
+                                                        serverOutput.println("Password should be min 8 characters long and contain min 2 digits");
+                                                        serverOutput.print("Choose your password: ");
+                                                        //newPassword = ClientHandler.RecordUserInput(userInput);
+                                                        newPassword = userInput.readLine();
 
-    	try {
-			File file = new File("./users.xml");
-			FileInputStream fileInput = new FileInputStream(file);
-			Properties properties = new Properties();
-			properties.loadFromXML(fileInput);
-			fileInput.close();
+                                                        tries += 1;
+                                                }
 
-			String userPass = properties.getProperty(nick);
-
-	         if (! password.equals(userPass)) {
-	        	 System.out.println("LOG: User entered incorrect password");
-	        	 return false;
-         }
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-        return true;  
-    }
-
-    static boolean isValid(String password) { 
-      
-        if (password.length() < 8) {   
-            return false;  
-        } else {      
-            char c;  
-            int count = 0;   
-            for (int i = 0; i < password.length(); i++) {  
-                c = password.charAt(i);  
-                if (!Character.isLetterOrDigit(c)) {          
-                    return false;  
-                } else if (Character.isDigit(c)) {  
-                    count++;  
-                }  
-
-            }  
-
-            if (count < 2)   {     
-                return false;  
-            }     
+                                        }
+                                } catch(NullPointerException e) {
+                                        /* respuesta era un enter */
+                                }
+                        } 
+                }
         }
-        return true;
-    }
+
+        static boolean checkCredentials(String nick, String password) { 
+
+                if (password == null) return false;
+
+                try {
+                        File file = new File(USER_DATABASE);
+                        FileInputStream fileInput = new FileInputStream(file);
+                        Properties properties = new Properties();
+                        properties.loadFromXML(fileInput);
+                        fileInput.close();
+
+                        String userPass = properties.getProperty(nick);
+
+                        if (! password.equals(userPass)) {
+                                System.out.println("User entered incorrect password");
+                                return false;
+                        }
+
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+
+                return true;  
+        }
+
+        static boolean isValid(String password) { 
+
+                if (password.length() < 8) {   
+                        return false;  
+                } else {      
+                        char c;  
+                        int count = 0;   
+                        for (int i = 0; i < password.length(); i++) {  
+                                c = password.charAt(i);  
+                                if (!Character.isLetterOrDigit(c)) {          
+                                        return false;  
+                                } else if (Character.isDigit(c)) {  
+                                        count++;  
+                                }  
+
+                        }  
+
+                        if (count < 2)   {     
+                                return false;  
+                        }     
+                }
+                return true;
+        }
 }
