425271654292e47644e7d3c6067ef94353f03b29
mgs <magnus.henkel@zoho.com>
2013-12-02 03:41:18 -0600

    version estable, algunos bugs

--- proyecto_final/src/Server.java
+++ proyecto_final/src/Server.java
@@ -21,29 +21,28 @@ public class Server extends Thread{
 
 	private static final int PORT = 8888;
 	private static final String HISTORYLOG = "./chatlog.txt";
-	//private static final String BLACKLIST = "./banned.txt"; 
 	private static final String BLACKLIST = "./blacklist.xml"; 
 
 	static Socket client;
-    static ArrayList<ClientThread> clientList = new ArrayList<ClientThread>();
-    static ArrayList<String> chatLog = new ArrayList<String>();
-    static ArrayList<String> usersBanned = new ArrayList<String>();
+	static ArrayList<ClientThread> clientList = new ArrayList<ClientThread>();
+	static ArrayList<String> chatLog = new ArrayList<String>();
+	static ArrayList<String> usersBanned = new ArrayList<String>();
 	static ArrayList<String> usersOnline = new ArrayList<String>(); 
-    
-	 public static void addUsersBanned(ArrayList<String> usersBanned){
-         Scanner user = null;
-         try{
-             user = new Scanner(new File(BLACKLIST));
-             user.useDelimiter("\n");
-         }catch(FileNotFoundException e){
-             System.out.println("File not found");
-         }
-         while(user.hasNext()){
-             String contact = user.next();
-             usersBanned.add(contact.substring(contact.indexOf('*')+1,contact.indexOf('#')));
-         }
-         user.close();
-     }
+
+	public static void addUsersBanned(ArrayList<String> usersBanned){
+		Scanner user = null;
+		try{
+			user = new Scanner(new File(BLACKLIST));
+			user.useDelimiter("\n");
+		}catch(FileNotFoundException e){
+			System.out.println("File not found");
+		}
+		while(user.hasNext()){
+			String contact = user.next();
+			usersBanned.add(contact.substring(contact.indexOf('*')+1,contact.indexOf('#')));
+		}
+		user.close();
+	}
 
 	public static void readFileIntoArray(ArrayList<String> array, String file) {
 		Scanner readBuffer = null;
@@ -53,7 +52,7 @@ public class Server extends Thread{
 		} catch (FileNotFoundException e) {
 			System.out.println("ERROR: Logfile not found");
 		}
-		
+
 		while (readBuffer.hasNext()) {
 			String line = readBuffer.next();
 			array.add(line);
@@ -61,7 +60,7 @@ public class Server extends Thread{
 		readBuffer.close();
 	}
 
-	
+
 	@Override
 	public void start() {
 		boolean runServer = true;
@@ -71,33 +70,33 @@ public class Server extends Thread{
 		//addUsersBanned(usersBanned);
 
 		AdminActions admin = new AdminActions();
-        System.out.println("***Welcome to the Admin panel***");
+		System.out.println("***Welcome to the Admin panel***");
 
 		try {
 			final ServerSocket chatServer = new ServerSocket(PORT);
 
 			while(runServer == true) {
 				System.out.println("Server waiting for Clients on port " + PORT + ".");
-					
+
 				new Thread(new Runnable() {
-	                @Override
+					@Override
 					public void run() {
-	                    try {
-	                    	while (true) {
+						try {
+							while (true) {
 								client = chatServer.accept();
-								System.out.println("Client accepted");
+								//System.out.println("Client accepted");
 								ClientThread cThread = new ClientThread(client);
 								clientList.add(cThread);
 								cThread.start();
-			                   	}
+							}
 
-	                    } catch(IOException e) {
-	                    	e.printStackTrace();
-	                    }
-	                }
-	            }).start();
-				
-				
+						} catch(IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}).start();
+
+
 
 				runServer = admin.action();
 			} 
@@ -111,179 +110,195 @@ public class Server extends Thread{
 				System.out.println("Shutting down user connections");
 				try {
 					for(int i = 0; i < clientList.size(); ++i) {
-						//ClientThread closeThread = clientList.get(i);
-						ClientThread.serverOutput.println("Server shutting down in 4 seconds");
+						ClientThread closeThread = clientList.get(i);
+						//serverOutput.println("Server shutting down in 4 seconds");
 					}
-						Thread.sleep(4000);
+					Thread.sleep(4000);
 					for(int i = 0; i < clientList.size(); ++i) {
 						ClientThread closeThread = clientList.get(i);
-						ClientThread.serverOutput.println("Server shutting down in 4 seconds");
+						//serverOutput.println("Server shutting down in 4 seconds");
+						//closeThread.serverOutput.println("Server shutting down in 4 seconds");
 						try {
-							ClientThread.serverOutput.close();
-							ClientThread.userInput.close();
+							closeThread.serverOutput.close();
+							closeThread.userInput.close();
 							closeThread.socket.close(); 
 						} catch(Exception e) {
 							e.printStackTrace();
 						}
 					}
 				} catch(InterruptedException e) {
-				    Thread.currentThread().interrupt();
+					Thread.currentThread().interrupt();
 				}
 			}
 			System.exit(0);
 		}
 	}
-		
-		
-    public static void main(String args[]){
 
-        try{
-    	Server server = new Server();
-		server.start();
-            
-        }catch(Exception e){
-            System.out.println("An exception ocurred");
-            System.out.println(e);
-        }
-    }
-        
+
+	public static void main(String args[]){
+
+		try{
+			Server server = new Server();
+			server.start();
+
+		}catch(Exception e){
+			System.out.println("An exception ocurred");
+			System.out.println(e);
+		}
+	}
+
 }
-    class ClientThread extends Thread {
-		 /* los hilos para cada cliente que se conecta */
-		Socket socket;
-		static DataInputStream userInput;
-		static PrintStream serverOutput;
-		private String nickname;
-	    private String passwd;
-	    private String inputString;
-	    static int numSockets;
-	    private SimpleDateFormat timeStamp;
-	    //private String time;
-		private static final String HISTORYLOG = "chatlog.txt";
+class ClientThread extends Thread {
+	/* los hilos para cada cliente que se conecta */
+	Socket socket;
+	DataInputStream userInput;
+	PrintStream serverOutput;
+	private String nickname;
+	private String passwd;
+	private String inputString;
+	static int numSockets;
+	private SimpleDateFormat timeStamp;
+	private SimpleDateFormat timeStampChat;
+	//private String time;
+	private static final String HISTORYLOG = "chatlog.txt";
 
 
-		// Constructor
-		ClientThread(Socket socket) {
-			this.socket = socket;
-			timeStamp = new SimpleDateFormat("E MMM dd yyyy  HH:mm:ss  ");
+	// Constructor
+	ClientThread(Socket socket) {
+		this.socket = socket;
+		timeStamp = new SimpleDateFormat("E MMM dd yyyy  HH:mm:ss  ");
+		timeStampChat = new SimpleDateFormat("HH:mm:ss  ");
 
-			try
-			{
-				serverOutput = new PrintStream(socket.getOutputStream());
-				userInput  = new DataInputStream(socket.getInputStream());
-			}
-			catch (IOException e) {
-				System.out.println("Exception creating new Input/output Streams: " + e);
-				return;
-			}
+		try
+		{
+			serverOutput = new PrintStream(socket.getOutputStream());
+			userInput  = new DataInputStream(socket.getInputStream());
 		}
+		catch (IOException e) {
+			System.out.println("Exception creating new Input/output Streams: " + e);
+			return;
+		}
+	}
 
 
-		@Override
-		public void run() {
-            try {
-				serverOutput = new PrintStream(socket.getOutputStream());
-				userInput = new DataInputStream(socket.getInputStream());
+	@Override
+	public void run() {
+		try {
+			serverOutput = new PrintStream(socket.getOutputStream());
+			userInput = new DataInputStream(socket.getInputStream());
 
-				String time = timeStamp.format(new Date());
+			String time = timeStamp.format(new Date());
 
-				serverOutput.println("**********************************");
-				serverOutput.println("*** Welcome to the Chat Server ***");
-				serverOutput.println("**********************************");
-				serverOutput.println("Connecting on " + time);
+			serverOutput.println("**********************************");
+			serverOutput.println("*** Welcome to the Chat Server ***");
+			serverOutput.println("**********************************");
+			serverOutput.println("Connecting on " + time);
 
-	            serverOutput.print("Nick: ");
+			serverOutput.print("Nick: ");
 
-	            nickname = userInput.readLine();
-	            		
-	            Boolean signedUp = AccessTools.checkSignUp(nickname);
+			nickname = userInput.readLine();
 
-	            if (signedUp == false) {
-		            	AccessTools.SignUp(serverOutput, userInput, socket, nickname, numSockets);
-	            }
-	            
-//	            if(! socket.isClosed()) { 	/* si el usuario estaba registrado o se registró....*/
-		            serverOutput.print("Password: ");
-	
-		            passwd = userInput.readLine();
-	
-		            System.out.println("User entered password " + passwd);
+			Boolean signedUp = AccessTools.checkSignUp(nickname);
 
-		            /* Validación de la clave */
-					Boolean validPass = AccessTools.checkCredentials(nickname, passwd);
-					
-					System.out.println("socket closed last? " + socket.isClosed());
-					if (validPass == false) {
-						//System.out.println("socket alive? " + socket.isClosed());
-						serverOutput.println("Sorry, you have no access to this chat");
-						closeConnections(userInput, serverOutput, socket);
-						numSockets -= 1;
+			if (signedUp == false) {
+				AccessTools.SignUp(serverOutput, userInput, socket, nickname, numSockets);
+			}
+
+			//if(! socket.isClosed()) { 	/* si el usuario estaba registrado o se registró....*/
+			serverOutput.print("Password: ");
+
+			passwd = userInput.readLine();
+
+			System.out.println("User entered password " + passwd);
+
+			/* Validación de la clave */
+			Boolean validPass = AccessTools.checkCredentials(nickname, passwd);
+
+			//System.out.println("socket closed last? " + socket.isClosed());
+			if (validPass == false) {
+				//System.out.println("socket alive? " + socket.isClosed());
+				serverOutput.println("Sorry, you have no access to this chat");
+				closeConnections(userInput, serverOutput, socket);
+				numSockets -= 1;
+			}
+			nickname = Tools.capitalizeFirstLetter(nickname);
+
+			if (Server.usersOnline.contains(nickname)){
+				serverOutput.println(nickname + " is already online");
+				closeConnections(userInput, serverOutput, socket);
+			} else {
+				Server.usersOnline.add(nickname);
+				//break;
+				serverOutput.println("\nSERVER:\tHi " + nickname + "\n>> : ");
+
+				for(int i=0; i< Server.clientList.size(); i++) {
+					if(Server.clientList.get(i)!=null && Server.clientList.get(i)!= this) {
+						Server.clientList.get(i).serverOutput.print("\n++ " + nickname + " entered the room ++\n>> :");
 					}
-		            nickname = Tools.capitalizeFirstLetter(nickname);
-	
-		            serverOutput.println("\nSERVER:\tHi " + nickname + "\n>> : ");
-	
-	
-		            for(int i=0; i< Server.clientList.size(); i++) {
-		                if(Server.clientList.get(i)!=null && Server.clientList.get(i)!= this) {
-							Server.clientList.get(i);
-							ClientThread.serverOutput.print("\n++ " + nickname  + " entered the room ++\n>> :");
-						}
-		            }
-	
-	            if(! socket.isClosed()) { 	/* si el usuario estaba registrado o se registró....*/
-		            while(true) {
-		            	serverOutput.print(">> : ");
-		            	inputString = userInput.readLine();
-		            	
-		                if (! inputString.startsWith("/")) {
-		                	//time = timeStamp.format(new Date());
-		                	Server.chatLog.add(time + " " + nickname + ": " + inputString);
-			                for(int i=0; i<Server.clientList.size(); i++) {
-			                    if(Server.clientList.get(i)!=null && Server.clientList.get(i)!= this) {
-			                    	//Server.clientList.get(i).serverOutput.println("\n" + time + " " + nickname +": " + inputString + "\n>> : ");
-			                    	Server.clientList.get(i).serverMsg(inputString);
-			                    }
-			                }
-		                } else {
-		                	if (UserActions.ExitChat(inputString, nickname)) break;
-	
-		                		UserActions.CommandSwitch(inputString, nickname);
-		                }
-		            }
-	
-		            // Terminando la conxión del cliente después de /exit
-		            for(int i = 0; i < Server.clientList.size(); i++){
-		                if(Server.clientList.get(i) != null && Server.clientList.get(i) != this) {
-		                	Server.clientList.get(i);
-							ClientThread.serverOutput.print("\n++ " + nickname + " left ++\n>> : ");
-		                	closeConnections(userInput, serverOutput, socket);
-		                }
-		            }
-	            }
-
-	            Tools.WriteToFile(Server.chatLog, HISTORYLOG);
-            
-				} catch (IOException e) {
-					e.printStackTrace();
 				}
+			}
+
+			if(! socket.isClosed()) { 	/* si el usuario estaba registrado o se registró....*/
+				String timeChat = null;
+				while(true) {
+					serverOutput.print(">> : ");
+					inputString = userInput.readLine();
+
+					if (! inputString.startsWith("/")) {
+						time = timeStamp.format(new Date());
+						timeChat = timeStampChat.format(new Date());
+						Server.chatLog.add(time + " " + nickname + ": " + inputString);
+
+						for(int i=0; i<Server.clientList.size(); i++) {
+							if(Server.clientList.get(i)!=null && Server.clientList.get(i)!= this) {
+								Server.clientList.get(i).serverOutput.print("\n" + timeChat + " " + nickname +": " + inputString + "\n>> : ");
+							}
+
+						}
+					} else {
+
+						UserActions.CommandSwitch(serverOutput, inputString, nickname);
+
+						if (UserActions.ExitChat(serverOutput, inputString, nickname)) {
+							Server.usersOnline.remove(nickname);
+							closeConnections(userInput, serverOutput, socket);
+							break;
+						}
+
+					}
+				}
+
+				// Terminando la conxión del cliente después de /exit
+				for(int i = 0; i < Server.clientList.size(); i++){
+					if(Server.clientList.get(i) != null && Server.clientList.get(i) != this) {
+						Server.clientList.get(i).serverOutput.print("\n++ " + nickname + " left ++\n>> : ");
+						closeConnections(userInput, serverOutput, socket);
+					}
+				}
+
+				//Tools.WriteToFile(Server.chatLog, HISTORYLOG);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
 
 	private void serverMsg(String message) throws IOException {
 
-        String time = timeStamp.format(new Date());
+		String timeChat = timeStampChat.format(new Date());
 
 		if(!socket.isConnected()) {
 			closeConnections(userInput, serverOutput, socket);;
 		}
 
-		serverOutput.println("\n" + time + " " + nickname +": " + message + "\n>> : ");
-		serverOutput.println(time + message);
+		serverOutput.println("\n" + timeChat + " " + nickname +": " + message + "\n>> : ");
+		serverOutput.println(timeChat + message);
 	}
 
 
-    public static void closeConnections(DataInputStream userInput, PrintStream serverOutput, Socket socket) {
-    	try {
+	public static void closeConnections(DataInputStream userInput, PrintStream serverOutput, Socket socket) {
+		try {
 			serverOutput.println("User connection closed");
 			userInput.close();
 			serverOutput.close();
@@ -291,5 +306,5 @@ public class Server extends Thread{
 		} catch (IOException e) {
 			System.out.println("Can't close client connection");
 		}
-    }
+	}
 }
--- proyecto_final/src/UserActions.java
+++ proyecto_final/src/UserActions.java
@@ -5,87 +5,86 @@ public class UserActions {
 	private static final String HISTORYLOG = "./chatlog.txt";
 
 
-        //public static void CommandSwitch(PrintStream serverOutput, String inputString, ArrayList<String> chatLog, String nickname, ArrayList <String> usersOnline) {
-		public static void CommandSwitch(String inputString, String nickname) {
+	//public static void CommandSwitch(PrintStream serverOutput, String inputString, ArrayList<String> chatLog, String nickname, ArrayList <String> usersOnline) {
+	public static void CommandSwitch(PrintStream serverOutput, String inputString, String nickname) {
 
-            if (inputString.equals("/users")) {
-				 			UserActions.ShowUsers();
-            }
-            else if(inputString.equals("/hist")) {
-                    //UserActions.ShowHistory(serverOutput, chatLog);
-                    UserActions.ShowHistory();
+		if (inputString.equals("/users")) {
+			UserActions.ShowUsers(serverOutput);
+		}
+		else if(inputString.equals("/hist")) {
+			UserActions.ShowHistory(serverOutput);
 
-            }
-            else if(inputString.equals("/help")) {
-                UserActions.ShowHelp();
-            }
-            else if(inputString.startsWith("/private")){
-				UserActions.PrivateMenssage(inputString, nickname);
-            }
-        }
-
-        public static void ShowUsers() {
-    		PrintStream serverOutput = ClientThread.serverOutput;
-    		ArrayList<String> usersOnline = Server.usersOnline;
-
-            System.out.println("Connected users right now:");
-				System.out.println(usersOnline);
-            serverOutput.println("Connected users right now");
-				 serverOutput.println(usersOnline);
-        }
-
-	public static void ShowHistory() {
-		PrintStream serverOutput = ClientThread.serverOutput;
-		ArrayList<String> chatLog = Server.chatLog;
-		
-    	serverOutput.println("Showing chat history");
-    	serverOutput.println("++++++++++++++++++++");
-    	if(chatLog.isEmpty()) {
-    		Server.readFileIntoArray(chatLog, HISTORYLOG);
-    	}
-
-    	for (int j = 0; j < chatLog.size(); j++) {
-        	String temp = chatLog.get(j);
-        	serverOutput.println(temp);
+		}
+		else if(inputString.equals("/help")) {
+			UserActions.ShowHelp(serverOutput);
+		}
+		else if(inputString.startsWith("/private")){
+			UserActions.PrivateMenssage(serverOutput, inputString, nickname);
 		}
 	}
-	
-        
-        public static void ShowHelp() {
-        	PrintStream serverOutput = ClientThread.serverOutput;
-                serverOutput.println("*********************************");
-                serverOutput.println("Options:");
-                serverOutput.println("/hist\tshow chat history");
-                serverOutput.println("/users\tshow online users");
-                serverOutput.println("/help\tshow this help menu");
-                serverOutput.println("/exit\tdisconnect");
-                serverOutput.println("*********************************");
-        }
 
-        public static void PrivateMenssage(String inputString, String nickname) {
-    		PrintStream serverOutput = ClientThread.serverOutput;
-    		ArrayList<ClientThread> clientList = Server.clientList;
-    		ArrayList<String> usersOnline = Server.usersOnline;
+	public static void ShowUsers(PrintStream serverOutput) {
+		//PrintStream serverOutput = ClientThread.serverOutput;
+		ArrayList<String> usersOnline = Server.usersOnline;
 
-            String tmpInputString = inputString.substring(inputString.indexOf(' ')+1);
-            tmpInputString = Tools.capitalizeFirstLetter(tmpInputString);
+		System.out.println("Connected users right now:");
+		System.out.println(usersOnline);
+		serverOutput.println("Connected users right now:");
+		serverOutput.println(usersOnline);
+	}
 
-            if (usersOnline.indexOf(tmpInputString.substring(0,tmpInputString.indexOf(' '))) == -1){
-            	serverOutput.println("User not found");
-            }
-            else{
-            	//clientList.get(usersOnline.indexOf(tmpInputString.substring(0,tmpInputString.indexOf(' ')))).serverOutput.println(nickname+">> "+tmpInputString.substring(tmpInputString.indexOf(' ')));
-            }                    
-                
-        }
+	public static void ShowHistory(PrintStream serverOutput) {
+		//PrintStream serverOutput = ClientThread.serverOutput;
+		ArrayList<String> chatLog = Server.chatLog;
 
-        public static boolean ExitChat(String inputString, String nickname) {
-	        PrintStream serverOutput = ClientThread.serverOutput;
+		serverOutput.println("Showing chat history");
+		serverOutput.println("++++++++++++++++++++");
+		if(chatLog.isEmpty()) {
+			Server.readFileIntoArray(chatLog, HISTORYLOG);
+		}
 
-	        if(inputString.startsWith("/exit")) {
-	                serverOutput.println("SERVER:\tGoodbye " + nickname);
-	                return true;
-	        }
-	        return false;
-	        }
+		for (int j = 0; j < chatLog.size(); j++) {
+			String temp = chatLog.get(j);
+			serverOutput.println(temp);
+		}
+	}
+
+
+	public static void ShowHelp(PrintStream serverOutput) {
+		//PrintStream serverOutput = ClientThread.serverOutput;
+		serverOutput.println("*********************************");
+		serverOutput.println("Options:");
+		serverOutput.println("/hist\tshow chat history");
+		serverOutput.println("/users\tshow online users");
+		serverOutput.println("/help\tshow this help menu");
+		serverOutput.println("/exit\tdisconnect");
+		serverOutput.println("*********************************");
+	}
+
+	public static void PrivateMenssage(PrintStream serverOutput, String inputString, String nickname) {
+		//PrintStream serverOutput = ClientThread.serverOutput;
+		ArrayList<ClientThread> clientList = Server.clientList;
+		ArrayList<String> usersOnline = Server.usersOnline;
+
+		String tmpInputString = inputString.substring(inputString.indexOf(' ')+1);
+		tmpInputString = Tools.capitalizeFirstLetter(tmpInputString);
+
+		if (usersOnline.indexOf(tmpInputString.substring(0,tmpInputString.indexOf(' '))) == -1){
+			serverOutput.println("User not found");
+		}
+		else{
+			clientList.get(usersOnline.indexOf(tmpInputString.substring(0,tmpInputString.indexOf(' ')))).serverOutput.println(nickname+">> "+tmpInputString.substring(tmpInputString.indexOf(' ')));
+		}                    
+
+	}
+
+	public static boolean ExitChat(PrintStream serverOutput, String inputString, String nickname) {
+		//PrintStream serverOutput = ClientThread.serverOutput;
+
+		if(inputString.startsWith("/exit")) {
+			serverOutput.println("SERVER:\tGoodbye " + nickname);
+			return true;
+		}
+		return false;
+	}
 }
