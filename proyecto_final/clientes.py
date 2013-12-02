1f6f2066eaaa43d5b0430449e4da615b218b7718
César Alberto <cesaratj27@gmail.com>
2013-12-02 02:49:46 -0600

    clientes listo para cerrar conexiones

--- proyecto_final/clientes.py
+++ proyecto_final/clientes.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""	Created on: Friday 29 november 2013
+	Equipo:	César Alberto Trejo Juárez     cesaralberto@yandex.com
+		Magnus Henkel
+		Oscar Díaz
+		Brenda
+"""
+
 import threading
 import socket
 
@@ -14,10 +21,20 @@ class write(threading.Thread):
 		self.ownSocket = socket
 
 	def run(self):
-		while True:
-			self.message = raw_input()
-			self.ownSocket.send(self.message + "\n")
-		self.ownSocket.close()
+		try:
+			while True:
+				self.message = raw_input()
+				self.ownSocket.send(self.message + "\n")
+		except KeyboardInterrupt:
+			#print 'KeyboardInterrupt: Exit request at write class'
+			self.ownSocket.send("/None \n")
+			self.ownSocket.close()
+		except EOFError:
+			#print 'EOFError: Exite request at write class'
+			self.ownSocket.send("/None \n")
+			self.ownSocket.close()
+		finally:
+			self.ownSocket.close()
 
 class read(threading.Thread):
 	"""A class wich lets read messages from Server"""
@@ -27,10 +44,18 @@ class read(threading.Thread):
 		self.ownSocket = socket
 
 	def run(self):
-		while True:
-			self.message = self.ownSocket.recv(1024)
-			print self.message
-		self.ownSocket.close()
+		try:
+			while True:
+				self.message = self.ownSocket.recv(1024)
+				print self.message
+		except EOFError:
+			#print 'EOFError: Exit request at class read'
+			self.ownSocket.close()
+		except:
+			#print 'Except: Exit request at class read'
+			self.ownSocket.close()
+		finally:
+			self.ownSocket.close()
 
 
 if __name__ == "__main__":
@@ -38,6 +63,10 @@ if __name__ == "__main__":
 	mysocket.connect((HOST, PORT))
 
 	hilos = [write(mysocket), read(mysocket)]
-
-	for h in hilos:
-		h.start()
+	try:
+		for h in hilos:
+			h.start()
+	except EOFError:
+		mysocket.send("/LOGOUT \n")
+		mysocket.close()
+		sys.exit(0)
